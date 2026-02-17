1dd55708bc077c99be1d5995628f83d8c758595b
fatisowsx <fatisowsx@example.com>
2026-02-17 17:21:06 +0600

    updated settings

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xnbk3+fu+ugbpw$iu#bx%!mao6_@ps0j*3_x=x=vnyd_=lejod
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'blog-app.onrender.com']
 
 
 # Application definition
